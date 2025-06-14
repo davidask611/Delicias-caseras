1043c7f58eadf86f66f64d859c50ed694ee3ae14
David <davidask611@gmail.com>
2025-06-14 15:06:30 -0300

    "agregado de productos y fotos"

--- productos.js
+++ productos.js
@@ -1,16 +1,16 @@
 // Array de productos
 const productos = [
   { nombre: "Torta arco iris", precio: "$50.000", imagen: "Torta_arco_iris.jpg" },
-  { nombre: "Alfajor", precio: "$140", imagen: "alfajor.jpg" },
-  { nombre: "Brownie", precio: "$1500", imagen: "brownie.jpg" },
+  { nombre: "Brownie", precio: "$15000", imagen: "brownie.jpg" },
   { nombre: "Pepas", precio: "$1000", imagen: "Pepas.jpg" },
-  { nombre: "Pastaflora", precio: "$9000", imagen: "Pasta_frola.jpg" },
+  { nombre: "Budin inglés", precio: "$5000", imagen: "Budin inglés.jpg" },
+  { nombre: "Pastaflora", precio: "$9100", imagen: "Pasta_frola.jpg" },
   { nombre: "Budin de banana con nuez", precio: "$6500", imagen: "budin.jpg" },
   { nombre: "Cupcakes de zanahoria y naranja con nuez", precio: "$8500 La docena", imagen: "Cupcakes de zanahoria y naranja con nuez.jpg" },
-  { nombre: "tarta de durazno", precio: "$6500", imagen: "tarta de durazno.jpg" },
-  { nombre: "tarta de ricota", precio: "$6500", imagen: "tarta de ricota.jpg" },
-  { nombre: "Lemon pie", precio: "$6500", imagen: "Lemon pie.jpg" },
-  { nombre: "Budin de limón con glase", precio: "$6500", imagen: "Budin de limón con glase.jpg" },
+  { nombre: "tarta de durazno", precio: "$17.000", imagen: "tarta de durazno.jpg" },
+  { nombre: "tarta de ricota", precio: "$17.000", imagen: "tarta de ricota.jpg" },
+  { nombre: "Tarta crocante de manzana", precio: "$6500", imagen: "Tarta crocante de manzana.jpg" },
+  { nombre: "Budin de limón con glase", precio: "$6000", imagen: "Budin de limón con glase.jpg" },
   { nombre: "Pancitos saborizados", precio: "$6500", imagen: "Pancitos saborizados.jpg" },
   { nombre: "Pancitos chips", precio: "$6500", imagen: "Pancitos chips.jpg" },
   { nombre: "Scon de queso", precio: "$6500", imagen: "Scon de queso.jpg" },
@@ -41,10 +41,9 @@ productos.forEach(prod => {
 
 // Nuevo array de productos saludables
 const saludables = [
-  { nombre: "Barritas de avena y miel", precio: "$1200", imagen: "barrita_de_miel_limon.jpg" },
-  { nombre: "Budin de banana con nuez", precio: "$2500", imagen: "Budin de banana con nuez.jpg" },
-  { nombre: "Cookies de avena y banana", precio: "$800", imagen: "cookies_avena_banana.jpg" },
-  // Agrega más productos saludables aquí si deseas
+  { nombre: "Budin de banana con nuez con harina integral", precio: "$7500", imagen: "Budin de banana con nuez.jpg" },
+  { nombre: "Galletas de avena con harina integral", precio: "$3000", imagen: "Galletas de avena y harina integral.jpg" },
+  // Aquí puedes agregar más productos saludables si deseas
 ];
 
 const saludableGrid = document.getElementById('saludable-grid');
